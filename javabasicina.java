636c1aa413e128fe575e9235e0c02ab4a30b59ba
faisalakb74 <faisall.akbar@gmail.com>
2021-10-12 16:37:48 +0700

    Upload tugas java basic

---
+++ javabasicina.java
@@ -0,0 +1,21 @@
+public class javabasicina{
+    //function is a method
+    public static void main (String[] args){
+
+        String  firstname   = "Ina ";
+        String  lastname    = "Mulyajmi";
+        int     age         = 20;
+        String  phonenumber = "0857-5937-7530";
+        String  address     = "Cianjur";
+        char    bloodtype   = 'B';
+        String  hobby       = "Ghibah";
+
+        System.out.println("Nama           = "+firstname + lastname);
+        System.out.println("Umur           = "+age+"th");
+        System.out.println("Nomor Hp       = "+phonenumber);
+        System.out.println("Alamat         = "+address);
+        System.out.println("Golongan darah = "+bloodtype);
+        System.out.println("Hobi           = "+hobby);
+    }
+    
+}
